7f7cb8302b8e52758e5829d1ba14e6ba836222fd
febby <febbymargareta.fm@gmail.com>
2026-04-30 15:31:27 +0700

    Revisi Layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,19 +12,19 @@
     <div class="h-screen flex flex-col">
 
         <!-- HEADER -->
-        <header class="h-28 bg-[#212842] px-8 flex items-center justify-between">
+        <header class="h-15 bg-[#212842] px-6 flex items-center justify-between">
 
             <!-- LOGO -->
-            <div>
-                <h1 class="text-5xl font-extrabold text-[#F0E7D5]">GARUDA</h1>
+            <div class="w-48">
+                <h1 class="text-2xl font-extrabold text-[#F0E7D5]">GARUDA</h1>
             </div>
 
             <!-- PROFIL KANAN -->
-            <div class="flex items-center gap-5">
+            <div class=" w-48 flex justify-end items-center gap-3">
                 <img src="https://ui-avatars.com/api/?name=Admin&background=F0E7D5&color=212842&size=80"
-                    class="w-16 h-16 rounded-full">
+                    class="w-10 h-10 rounded-full">
 
-                <span class="text-4xl font-extrabold text-[#F0E7D5]">
+                <span class="text-xl font-extrabold text-[#F0E7D5]">
                     Admin
                 </span>
             </div>
@@ -35,16 +35,16 @@
         <div class="flex flex-1 overflow-hidden">
 
             <!-- SIDEBAR -->
-            <aside class="w-96 bg-[#F0E7D5] flex flex-col px-6 py-8">
+            <aside class="w-60 bg-[#F0E7D5] flex flex-col">
 
-                <h2 class="text-4xl font-extrabold text-[#212842] mb-6">
-                    Menu
-                </h2>
+                <div class="p-6 pb-3">
+                    <h2 class="text-2xl font-extrabold text-[#212842]">Menu</h2>
+                </div>
 
-                <div class="space-y-8">
+                <div class="px-4 space-y-3 overflow-y-auto" style="height: 300px;">
 
                     <a href="{{ url('/admin/dashboard') }}"
-                        class="block px-7 py-5 text-3xl font-bold transition duration-200
+                        class="block px-4 py-2 text-xl font-bold transition duration-200
             {{ request()->is('admin/dashboard')
                 ? 'bg-[#212842] text-[#F0E7D5]'
                 : 'text-[#212842] hover:bg-[#212842] hover:text-[#F0E7D5]' }}">
@@ -52,7 +52,7 @@
                     </a>
 
                     <a href="{{ url('/admin/rekapitulasi') }}"
-                        class="block px-7 py-5 text-3xl font-bold transition duration-200
+                        class="block px-4 py-2 text-xl font-bold transition duration-200
             {{ request()->is('admin/rekapitulasi')
                 ? 'bg-[#212842] text-[#F0E7D5]'
                 : 'text-[#212842] hover:bg-[#212842] hover:text-[#F0E7D5]' }}">
@@ -61,15 +61,20 @@
 
                 </div>
 
-                <div class="mt-auto">
-                    <div id="tanggal" class="text-2xl font-bold mb-3 text-[#212842]"></div>
-                    <div id="jam" class="text-5xl font-extrabold text-[#212842]"></div>
+                <!-- BAWAH SIDEBAR -->
+                <div class="mt-auto p-6 ">
+
+                    <div class="mt-6 pt-5">
+                        <div id="tanggal" class="text-base font-bold mb-2 text-[#212842]"></div>
+                        <div id="jam" class="text-2xl font-extrabold text-[#212842]"></div>
+                    </div>
+
                 </div>
 
             </aside>
 
             <!-- CONTENT -->
-            <main class="flex-1 p-10 overflow-y-auto">
+            <main class="flex-1 p-6 overflow-y-auto">
                 @yield('content')
             </main>
 
--- resources/views/layouts/kasir.blade.php
+++ resources/views/layouts/kasir.blade.php
@@ -12,18 +12,18 @@
     <div class="h-screen flex flex-col">
 
         <!-- HEADER -->
-        <header class="h-28 bg-[#212842] px-8 flex items-center">
+        <header class="h-15 bg-[#212842] px-6 flex items-center">
 
             <!-- LOGO -->
-            <div class="w-72">
-                <h1 class="text-5xl font-extrabold text-[#F0E7D5]">GARUDA</h1>
+            <div class="w-48">
+                <h1 class="text-2xl font-extrabold text-[#F0E7D5]">GARUDA</h1>
             </div>
 
             <!-- TAB MENU -->
-            <nav class="flex-1 flex justify-center gap-8 text-4xl font-extrabold">
+            <nav class="flex-1 flex justify-center gap-5 text-xl font-extrabold">
 
                 <a href="{{ url('/kasir/transaksi') }}"
-                    class="px-8 py-4 rounded-xl shadow-md transition duration-200
+                    class="px-5 py-3 rounded-xl shadow-md transition duration-200
     {{ request()->is('kasir/transaksi')
         ? 'bg-[#F0E7D5] text-[#212842]'
         : 'bg-[#212842] text-[#F0E7D5] hover:bg-[#F0E7D5] hover:text-[#212842]' }}">
@@ -31,7 +31,7 @@
                 </a>
 
                 <a href="{{ url('/kasir/stok') }}"
-                    class="px-8 py-4 rounded-xl shadow-md transition duration-200
+                    class="px-5 py-3 rounded-xl shadow-md transition duration-200
     {{ request()->is('kasir/stok')
         ? 'bg-[#F0E7D5] text-[#212842]'
         : 'bg-[#212842] text-[#F0E7D5] hover:bg-[#F0E7D5] hover:text-[#212842]' }}">
@@ -39,7 +39,7 @@
                 </a>
 
                 <a href="{{ url('/kasir/rekapitulasi') }}"
-                    class="px-8 py-4 rounded-xl shadow-md transition duration-200
+                    class="px-5 py-3 rounded-xl shadow-md transition duration-200
     {{ request()->is('kasir/rekapitulasi')
         ? 'bg-[#F0E7D5] text-[#212842]'
         : 'bg-[#212842] text-[#F0E7D5] hover:bg-[#F0E7D5] hover:text-[#212842]' }}">
@@ -49,9 +49,9 @@
             </nav>
 
             <!-- PROFIL -->
-            <div class="w-72 flex justify-end items-center gap-5">
-                <img src="" class="w-16 h-16 rounded-full">
-                <span class="text-4xl font-extrabold text-[#F0E7D5]">Kasir</span>
+            <div class="w-48 flex justify-end items-center gap-3">
+                <img src="" class="w-10 h-10 rounded-full">
+                <span class="text-xl font-extrabold text-[#F0E7D5]">Kasir</span>
             </div>
 
         </header>
@@ -60,18 +60,18 @@
         <div class="flex flex-1 overflow-hidden">
 
             <!-- SIDEBAR -->
-            <aside class="w-96 bg-[#F0E7D5] flex flex-col">
+            <aside class="w-60 bg-[#F0E7D5] flex flex-col">
 
                 <!-- JUDUL -->
-                <div class="p-8 pb-4">
-                    <h2 class="text-4xl font-extrabold text-[#212842]">Rombel</h2>
+                <div class="p-6 pb-3">
+                    <h2 class="text-2xl font-extrabold text-[#212842]">Rombel</h2>
                 </div>
 
                 <!-- LIST ROMBEL: hanya 5 yang terlihat, sisanya scroll -->
-                <div class="px-6 space-y-4 overflow-y-auto" style="height: 500px;">
+                <div class="px-4 space-y-3 overflow-y-auto" style="height: 300px;">
 
                     <a href="{{ request()->fullUrlWithQuery(['rombel' => 'graha']) }}"
-                        class="block px-7 py-5 text-3xl font-bold transition duration-200
+                        class="block px-4 py-2 text-xl font-bold transition duration-200
     {{ request('rombel', 'graha') == 'graha'
         ? 'bg-[#212842] text-[#F0E7D5] border-l-8 border-[#F0E7D5]'
         : 'bg-[#F0E7D5] text-[#212842] hover:bg-[#212842] hover:text-[#F0E7D5]' }}">
@@ -79,7 +79,7 @@
                     </a>
 
                     <a href="{{ request()->fullUrlWithQuery(['rombel' => 'membatik']) }}"
-                        class="block px-7 py-5 text-3xl font-bold transition duration-200
+                        class="block px-4 py-2 text-xl font-bold transition duration-200
     {{ request('rombel') == 'membatik'
         ? 'bg-[#212842] text-[#F0E7D5] border-l-8 border-[#F0E7D5]'
         : 'bg-[#F0E7D5] text-[#212842] hover:bg-[#212842] hover:text-[#F0E7D5]' }}">
@@ -87,7 +87,7 @@
                     </a>
 
                     <a href="{{ request()->fullUrlWithQuery(['rombel' => 'perkayuan']) }}"
-                        class="block px-7 py-5 text-3xl font-bold transition duration-200
+                        class="block px-4 py-2 text-xl font-bold transition duration-200
     {{ request('rombel') == 'perkayuan'
         ? 'bg-[#212842] text-[#F0E7D5] border-l-8 border-[#F0E7D5]'
         : 'bg-[#F0E7D5] text-[#212842] hover:bg-[#212842] hover:text-[#F0E7D5]' }}">
@@ -95,7 +95,7 @@
                     </a>
 
                     <a href="{{ request()->fullUrlWithQuery(['rombel' => 'busana']) }}"
-                        class="block px-7 py-5 text-3xl font-bold transition duration-200
+                        class="block px-4 py-2 text-xl font-bold transition duration-200
     {{ request('rombel') == 'busana'
         ? 'bg-[#212842] text-[#F0E7D5] border-l-8 border-[#F0E7D5]'
         : 'bg-[#F0E7D5] text-[#212842] hover:bg-[#212842] hover:text-[#F0E7D5]' }}">
@@ -103,7 +103,7 @@
                     </a>
 
                     <a href="{{ request()->fullUrlWithQuery(['rombel' => 'tata-boga']) }}"
-                        class="block px-7 py-5 text-3xl font-bold transition duration-200
+                        class="block px-4 py-2 text-xl font-bold transition duration-200
     {{ request('rombel') == 'tata-boga'
         ? 'bg-[#212842] text-[#F0E7D5] border-l-8 border-[#F0E7D5]'
         : 'bg-[#F0E7D5] text-[#212842] hover:bg-[#212842] hover:text-[#F0E7D5]' }}">
@@ -111,7 +111,7 @@
                     </a>
 
                     <a href="{{ request()->fullUrlWithQuery(['rombel' => 'kecantikan']) }}"
-                        class="block px-7 py-5 text-3xl font-bold transition duration-200
+                        class="block px-4 py-2 text-xl font-bold transition duration-200
     {{ request('rombel') == 'kecantikan'
         ? 'bg-[#212842] text-[#F0E7D5] border-l-8 border-[#F0E7D5]'
         : 'bg-[#F0E7D5] text-[#212842] hover:bg-[#212842] hover:text-[#F0E7D5]' }}">
@@ -119,7 +119,7 @@
                     </a>
 
                     <a href="{{ request()->fullUrlWithQuery(['rombel' => 'logam']) }}"
-                        class="block px-7 py-5 text-3xl font-bold transition duration-200
+                        class="block px-4 py-2 text-xl font-bold transition duration-200
     {{ request('rombel') == 'logam'
         ? 'bg-[#212842] text-[#F0E7D5] border-l-8 border-[#F0E7D5]'
         : 'bg-[#F0E7D5] text-[#212842] hover:bg-[#212842] hover:text-[#F0E7D5]' }}">
@@ -132,13 +132,13 @@
                 <div class="mt-auto p-6 ">
 
                     <button
-                        class="w-full bg-[#CA0B00] text-[#F0E7D5] text-3xl font-extrabold py-5 rounded-xl hover:bg-red-700">
+                        class="w-full bg-[#CA0B00] text-[#F0E7D5] text-xl font-extrabold py-2 rounded-lg hover:bg-red-700">
                         Tutup Kasir
                     </button>
 
                     <div class="mt-6 pt-5">
-                        <div id="tanggal" class="text-2xl font-bold mb-3 text-[#212842]"></div>
-                        <div id="jam" class="text-5xl font-extrabold text-[#212842]"></div>
+                        <div id="tanggal" class="text-base font-bold mb-2 text-[#212842]"></div>
+                        <div id="jam" class="text-2xl font-extrabold text-[#212842]"></div>
                     </div>
 
                 </div>
@@ -146,7 +146,7 @@
             </aside>
 
             <!-- CONTENT -->
-            <main class="flex-1 p-10 overflow-y-auto">
+            <main class="flex-1 p-6 overflow-y-auto">
                 @yield('content')
             </main>
 
