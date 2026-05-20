733ae206d9805f284fe5a6e8ac0ac6e2adccbb3c
febby <febbymargareta.fm@gmail.com>
2026-05-20 15:30:50 +0700

    revisi tgljam admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -43,21 +43,25 @@
                     <div class="flex items-center justify-end gap-1">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-[#F0E7D5]" fill="none"
                             viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M8 7V3m8 4V3m-9 8h10m-11 9h12a2 2 0 002-2V7a2 2 0 00-2-2H6a2 2 0 00-2 2v11a2 2 0 002 2z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10m-11
+                    9h12a2 2 0 002-2V7a2 2 0
+                    00-2-2H6a2 2 0 00-2 2v11a2
+                    2 0 002 2z" />
                         </svg>
 
-                        <div id="tanggal" class="text-sm font-bold text-[#F0E7D5]"></div>
+                        <div id="tanggal" class="text-sm text-[#F0E7D5]">
+                        </div>
                     </div>
 
                     <div class="flex items-center justify-end gap-1 mt-0.5">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-[#F0E7D5]" fill="none"
                             viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0
+                    11-18 0 9 9 0 0118 0z" />
                         </svg>
 
-                        <div id="jam" class="text-sm font-extrabold text-[#F0E7D5]"></div>
+                        <div id="jam" class="text-sm text-[#F0E7D5]">
+                        </div>
                     </div>
                 </div>
 
